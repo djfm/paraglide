328c7348b5f159697d0a1c9d4412c40d342de711
djfm <fm.de.jouvencel@gmail.com>
2016-08-15 16:33:20 +0200

    Recognized nodes can be tagged.

--- test/index.js
+++ test/index.js
@@ -1,25 +1,58 @@
 const {
-  digit,
-} = require('../lib/grammar');
+  charParser,
+  tag,
+  runParser,
+} = require('../lib/parsing');
 
-describe('A digit parser', () => {
+describe('A char parser for "1"', () => {
+  const p = runParser(charParser('1'));
+
   it('parses the digit "1"', () =>
-    digit('1').should.deep.equal({
+    p('1').should.deep.equal({
       recognized: true,
       nodes: ['1'],
     })
   );
   it('doesn\'t parse the string "a"', () =>
-    digit('a').should.deep.equal({
+    p('a').should.deep.equal({
       recognized: false,
       nodes: ['a'],
     })
   );
   it('parses the digit "1" in "1ab" but leaves the "ab"', () =>
-    digit('1ab').should.deep.equal({
+    p('1ab').should.deep.equal({
       recognized: true,
       nodes: [{
         recognized: true,
+        nodes: ['1'],
+      }, {
+        recognized: false,
+        nodes: ['a', 'b'],
+      }],
+    })
+  );
+});
+
+describe('A tagged char parser for "1"', () => {
+  const p = runParser(tag('one')(charParser('1')));
+
+  it('parses the digit "1"', () =>
+    p('1').should.deep.equal({
+      recognized: 'one',
+      nodes: ['1'],
+    })
+  );
+  it('doesn\'t parse the string "a"', () =>
+    p('a').should.deep.equal({
+      recognized: false,
+      nodes: ['a'],
+    })
+  );
+  it('parses the digit "1" in "1ab" but leaves the "ab"', () =>
+    p('1ab').should.deep.equal({
+      recognized: true,
+      nodes: [{
+        recognized: 'one',
         nodes: ['1'],
       }, {
         recognized: false,
