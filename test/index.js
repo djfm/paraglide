20a4533d6828a3d3efd65a8dcf57d59c50bce373
djfm <fm.de.jouvencel@gmail.com>
2016-08-19 07:34:46 +0200

    First steps towards the "sequence" combinator

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 const {
   charParser,
+  sequence,
 } = require('../lib/parsing');
 
 describe('A "charParser" for the char "a"', () => {
@@ -27,4 +28,31 @@ describe('A "charParser" for the char "a"', () => {
       }],
     })
   );
+});
+
+describe('The "sequence" combinator', () => {
+  describe('a sequence made of parsers for "a" and "b"', () => {
+    const a = charParser('a');
+    const b = charParser('b');
+
+    const ab = sequence(a, b);
+
+    it('does not recognize the string "a"', () =>
+      ab('a').should.deep.equal({
+        tag: { recognized: false },
+        nodes: [
+          { tag: { recognized: false }, nodes: ['a'] },
+        ],
+      })
+    );
+
+    it('recognizes the string "ab"', () =>
+      ab('a').should.deep.equal({
+        tag: { recognized: true },
+        nodes: [
+          { tag: { recognized: true }, nodes: ['a', 'b'] },
+        ],
+      })
+    );
+  });
 });
