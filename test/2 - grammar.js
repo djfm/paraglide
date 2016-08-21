aa226205255e094380d3c4516d8e392c24d37822
djfm <fm.de.jouvencel@gmail.com>
2016-08-21 15:55:09 +0200

    "floating" parser

---
+++ test/2 - grammar.js
@@ -0,0 +1,62 @@
+const {
+  digit,
+  integer,
+  floating,
+} = require('../lib/grammar');
+
+describe('The grammar', () => {
+  describe('digit is an untyped parser', () => {
+    it('recognizes digits in the range 0-9', () =>
+      digit('4').should.deep.equal({
+        tag: { recognized: true },
+        nodes: [{ tag: { recognized: true }, nodes: ['4'] }],
+      })
+    );
+  });
+
+  describe('integer is a typed parser', () => {
+    it('recognizes "4"', () =>
+      integer('4').should.deep.equal({
+        tag: { recognized: true },
+        nodes: [{ tag: { recognized: true, type: 'integer' }, nodes: ['4'] }],
+      })
+    );
+
+    it('recognizes "42"', () =>
+      integer('42').should.deep.equal({
+        tag: { recognized: true },
+        nodes: [
+          {
+            tag: { recognized: true, type: 'integer' },
+            nodes: ['4', '2'],
+          },
+        ],
+      })
+    );
+  });
+
+  describe('floating is a typed parser', () => {
+    it('does not recognize "4"', () =>
+      floating('4').should.deep.equal({
+        tag: { recognized: false },
+        nodes: [{ tag: { recognized: false }, nodes: ['4'] }],
+      })
+    );
+
+    it('recognizes "4.2"', () =>
+      floating('4.2').should.deep.equal({
+        tag: { recognized: true },
+        nodes: [
+          {
+            tag: { recognized: true, type: 'floating' },
+            nodes: [
+              { tag: { recognized: true, type: 'integer' }, nodes: ['4'] },
+              '.',
+              { tag: { recognized: true, type: 'integer' }, nodes: ['2'] },
+            ],
+          },
+        ],
+      })
+    );
+  });
+});
